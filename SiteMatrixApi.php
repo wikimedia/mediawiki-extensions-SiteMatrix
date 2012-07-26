defd30c5411070d6f006719ebd0435046276551e
Max Semenik <maxsem.wiki@gmail.com>
2012-07-26 17:22:58 +0400

    Make action=sitematrix more flexible
    Allow to return only the information needed, introduce paging.
    This is needed to reduce memory consumption of our J2ME app.
    
    Change-Id: I6f775667a99c3f2aab0e2b4882a143cb178b7dc8

--- SiteMatrixApi.php
+++ SiteMatrixApi.php
@@ -29,15 +29,32 @@ class ApiQuerySiteMatrix extends ApiQueryBase {
 		$params = $this->extractRequestParams();
 		$type = array_flip( $params['type'] );
 		$state = array_flip( $params['state'] );
+		$langProp = array_flip( $params['langprop'] );
+		$siteProp = array_flip( $params['siteprop'] );
+		$limit = $params['limit'];
+		$continue = isset( $params['continue'] )
+			? explode( '|', $params['continue'] )
+			: array( 'language', '' );
+		if ( count( $continue ) != 2 ) {
+			$this->dieUsage( 'Invalid continue param. You should pass the original value returned by the previous query', 'badcontinue' );
+		}
 
 		$all = isset( $state['all'] );
 		$closed = isset( $state['closed'] );
 		$private = isset( $state['private'] );
 		$fishbowl = isset( $state['fishbowl'] );
 
-		if ( isset( $type['language'] ) ) {
+		$count = 0;
+		if ( isset( $type['language'] ) && $continue[0] == 'language' ) {
 			foreach ( $matrix->getLangList() as $lang ) {
 				$langhost = str_replace( '_', '-', $lang );
+				if ( $langhost < $continue[1] ) {
+					continue;
+				}
+				if ( $count >= $limit ) {
+					$this->setContinueEnumParameter( 'continue', "language|$langhost" );
+					break;
+				}
 				$language = array(
 					'code' => $langhost,
 					'name' => $langNames[$lang],
@@ -46,37 +63,42 @@ class ApiQuerySiteMatrix extends ApiQueryBase {
 				if ( isset( $localLanguageNames[$lang] ) ) {
 					$language['localname'] = $localLanguageNames[$lang];
 				}
+				$language = array_intersect_key( $language, $langProp );
 
-				foreach ( $matrix->getSites() as $site ) {
-					if ( $matrix->exist( $lang, $site ) ) {
-						$skip = true;
+				if ( isset( $language['site'] ) ) {
+					foreach ( $matrix->getSites() as $site ) {
+						if ( $matrix->exist( $lang, $site ) ) {
+							$skip = true;
 
-						if ( $all ) {
-							$skip = false;
-						}
-
-						$url = $matrix->getCanonicalUrl( $lang, $site );
-						$site_out = array(
-							'url' => $url,
-							'dbname' => $matrix->getDBName( $lang, $site ),
-							'code' => $site,
-							'sitename' => $matrix->getSitename( $lang, $site ),
-						);
-						if ( $matrix->isClosed( $lang, $site ) ) {
-							$site_out['closed'] = '';
-							if ( $closed ) {
+							if ( $all ) {
 								$skip = false;
 							}
-						}
 
-						if ( $skip ) {
-							continue;
+							$url = $matrix->getCanonicalUrl( $lang, $site );
+							$site_out = array(
+								'url' => $url,
+								'dbname' => $matrix->getDBName( $lang, $site ),
+								'code' => $site,
+								'sitename' => $matrix->getSitename( $lang, $site ),
+							);
+							$site_out = array_intersect_key( $site_out, $siteProp );
+							if ( $matrix->isClosed( $lang, $site ) ) {
+								$site_out['closed'] = '';
+								if ( $closed ) {
+									$skip = false;
+								}
+							}
+
+							if ( $skip ) {
+								continue;
+							}
+							$language['site'][] = $site_out;
 						}
-						$language['site'][] = $site_out;
 					}
+					$result->setIndexedTagName( $language['site'], 'site' );
 				}
 
-				$result->setIndexedTagName( $language['site'], 'site' );
+				$count++;
 				$matrix_out[] = $language;
 			}
 		}
@@ -84,16 +106,23 @@ class ApiQuerySiteMatrix extends ApiQueryBase {
 		$result->setIndexedTagName( $matrix_out, 'language' );
 		$result->addValue( null, "sitematrix", $matrix_out );
 
-		if ( isset( $type['special'] ) ) {
-
+		if ( isset( $type['special'] ) && $count < $limit ) {
 			$specials = array();
 			foreach ( $matrix->getSpecials() as $special ){
 				list( $lang, $site ) = $special;
+				$dbName = $matrix->getDBName( $lang, $site );
+				if ( $continue[0] == 'special' && $dbName < $continue[1] ) {
+					continue;
+				}
+				if ( $count >= $limit ) {
+					$this->setContinueEnumParameter( 'continue', "special|$dbName" );
+					break;
+				}
 				$url = $matrix->getCanonicalUrl( $lang, $site );
 
 				$wiki = array();
 				$wiki['url'] = $url;
-				$wiki['dbname'] = $matrix->getDBName( $lang, $site );
+				$wiki['dbname'] = $dbName;
 				$wiki['code'] = str_replace( '_', '-', $lang ) . ( $site != 'wiki' ? $site : '' );
 
 				$skip = true;
@@ -135,6 +164,15 @@ class ApiQuerySiteMatrix extends ApiQueryBase {
 		}
 	}
 
+	protected function setContinueEnumParameter( $paramName, $paramValue ) {
+		$paramName = $this->encodeParamName( $paramName );
+		$msg = array( $paramName => $paramValue );
+		$result = $this->getResult();
+		$result->disableSizeCheck();
+		$result->addValue( 'query-continue', $this->getModuleName(), $msg );
+		$result->enableSizeCheck();
+	}
+
 	public function getAllowedParams() {
 		return array(
 			'type' => array(
@@ -154,29 +192,68 @@ class ApiQuerySiteMatrix extends ApiQueryBase {
 					'fishbowl'
 				),
 				ApiBase::PARAM_DFLT => 'all',
-			)
+			),
+			'langprop' => array(
+				ApiBase::PARAM_ISMULTI => true,
+				ApiBase::PARAM_TYPE => array(
+					'code',
+					'name',
+					'site',
+					'localname',
+				),
+				ApiBase::PARAM_DFLT => 'code|name|site|localname',
+			),
+			'siteprop' => array(
+				ApiBase::PARAM_ISMULTI => true,
+				ApiBase::PARAM_TYPE => array(
+					'url',
+					'dbname',
+					'code',
+					'sitename',
+				),
+				ApiBase::PARAM_DFLT => 'url|dbname|code|sitename',
+			),
+			'limit' => array(
+				ApiBase::PARAM_DFLT => 5000,
+				ApiBase::PARAM_TYPE => 'limit',
+				ApiBase::PARAM_MIN => 1,
+				ApiBase::PARAM_MAX => 5000,
+				ApiBase::PARAM_MAX2 => 5000,
+			),
+			'continue' => null,
 		);
 	}
 
 	public function getParamDescription() {
 		return array(
-			'type' => 'Filter the Site Matrix by type',
-			'state' => 'Filter the Site Matrix by wiki state',
+			'type' => array(
+				'Filter the Site Matrix by type',
+				' special  - One off, and multilingual Wikimedia projects',
+				' language - Wikimedia projects under this language code',
+			),
+			'state' => array(
+				'Filter the Site Matrix by wiki state',
+				' closed   - No write access, full read access',
+				' private  - Read and write restricted',
+				' fishbowl - Restricted write access, full read access',
+			),
+			'langprop' => 'Which information about a language to return',
+			'siteprop' => 'Which information about a site to return',
+			'limit' => 'Maximum number of results',
+			'continue' => 'When more results are available, use this to continue',
 		);
+	}
+
+	public function getPossibleErrors() {
+		return array_merge( parent::getPossibleErrors(), array(
+			array( 'code' => 'badcontinue', 'info' => 'Invalid continue param. You should pass the original value returned by the previous query' ),
+		) );
 	}
 
 	public function getDescription() {
 		return array(
 			'Get Wikimedia sites list',
 			'The code is either the unique identifier for specials else, for languages, the project code',
-			'',
-			'Wiki types:',
-			' special  - One off, and multilingual Wikimedia projects',
-			' language - Wikimedia projects under this language code',
-			'Wiki states:',
-			' closed   - No write access, full read access',
-			' private  - Read and write restricted',
-			' fishbowl - Restricted write access, full read access',
 			);
 	}
 
