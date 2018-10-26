8a07c0f8f8f07b7508f6d40e1cf074691afb8e61
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 21:56:21 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I6b0f07e4b59f9d93a962f45fbcc8213ba11c28fb

--- SiteMatrix.php
+++ SiteMatrix.php
@@ -8,10 +8,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['SiteMatrix'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['SiteMatrixAliases'] = __DIR__ . '/SiteMatrix.alias.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for SiteMatrix extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return true;
 } else {
 	die( 'This version of the SiteMatrix extension requires MediaWiki 1.25+' );
